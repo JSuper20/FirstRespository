ea5887074850a63f076888d94885a28ff27ac1fb
damahou <393495856@qq.com>
2020-10-30 15:35:15 +0800

    idea提交

---
+++ Exercise/src/main/java/com/offcn/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.offcn.demo;
+
+public class Demo {
+
+    int a = 1;
+    double b = 2;
+
+    public void submit(int a, double b) {
+        double c = a + b;
+        System.out.println(c);
+    }
+}
